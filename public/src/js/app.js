55131b757340dd27f47ecb0f0a905ad372ab14aa
Nobuyuki Fujioka <n.fujioka@abacusmaths.info>
2017-11-20 22:27:02 +0000

    adds differed prompt for install app.

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var differedPrompt
+
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
@@ -5,3 +7,10 @@ if('serviceWorker' in navigator) {
       console.log('Service worker registered!')
     })
 }
+
+window.addEventListener('beforeinstallprompt', function(event){
+  console.log("beforeinstallprompt fired")
+  event.preventDefault()
+  defferedPrompt = event
+  return false
+})
